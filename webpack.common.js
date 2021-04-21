94cb8e7991dcdb145c38dda267e1b2e4b8611c1e
4betterweb <8webdevtest@gmail.com>
2021-04-21 15:40:43 +0200

    Added Babel and core-js pollyfill

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,11 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
         ],
     },
     plugins: [
