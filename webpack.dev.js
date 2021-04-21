23e44581cef4ab535a99c2ca2b28792b8a4226c7
4betterweb <8webdevtest@gmail.com>
2021-04-21 15:18:24 +0200

    Added processing: css and scss files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,2 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(sc|c)ss$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    }
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,2 +1,26 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(sc|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ],
+});
